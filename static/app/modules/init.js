61e20643499100318c6a53c06e719b05b8d8bdd3
tim <timford88@gmail.com>
2014-05-12 17:20:25 +0000

    Fix canvas-wrap not sizing properly

--- static/app/modules/init.js
+++ static/app/modules/init.js
@@ -1,6 +1,6 @@
 define(['jquery', 'app'], function(){
 	$(function(){
-		$('.canvas-wrap').height(window.innerHeight - ($('.navbar').outerHeight() + 20))
+		$('.canvas-wrap').height(window.innerHeight - ($('.navbar').outerHeight() + $('.controls').outerHeight()))
 			.css({'margin-top': $('.navbar').outerHeight()});
 		app.execute('stage:resize');
 	});
