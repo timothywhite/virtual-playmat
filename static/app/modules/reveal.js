626b5ddbf2b893f0d5a5ea5b03a230804996a63d
tim <timford88@gmail.com>
2014-06-04 15:06:07 +0000

    Change reveal from using multiple squares to using one custom shape

--- static/app/modules/reveal.js
+++ static/app/modules/reveal.js
@@ -1,59 +1,75 @@
 define(['app', 'kinetic', 'module/layers'], function(app, Kinetic){
         app.module('Ui', function(Ui, app, Backbone, Marionette, $, _){
 		var isRevealing = false,
-		_init_square = function(square){
-			
-			if (app.request('adventure:isjoined') && !app.request('adventure:isdm')){
-				square.fill('white');
-				square.stroke('grey');
-				square.opacity(1);
+		squares = [],
+		hideShape = new Kinetic.Shape({
+			x:0,
+			y:0,
+			stroke: 'grey',
+			fill: 'grey',
+			opacity: .5,
+			drawFunc: function(context){
+				var i, square;
+
+				if (app.request('adventure:isjoined') && !app.request('adventure:isdm')){
+					this.fill('white');
+					this.stroke('grey');
+					this.opacity(1);
+				}
+
+				context.beginPath();
+				for (i = 0; i < squares.length; i += 1){
+					square = squares[i];
+					context.moveTo(square[0], square[1]);
+					context.lineTo(square[2], square[1]);
+					context.lineTo(square[2], square[3]);
+					context.lineTo(square[0], square[3]);
+					context.lineTo(square[0], square[1]);
+				}
+				context.fillStrokeShape(this);
 			}
-			square.on('mousedown', function(e){
-				if (app.request('config', 'toolMode') === 'reveal'){
-                                	this.remove();
-					isRevealing = true;
-                                	app.request('layer','reveal').draw();
-					app.vent.trigger('reveal:update');
-				}
-				return true;
-                        });
-			square.on('mousemove', function(e){
-				if (isRevealing){
-					this.remove();
-					app.request('layer', 'reveal').draw();
-					app.vent.trigger('reveal:update');
-				}
-			});
-			square.on('mouseup', function(e){
-				app.execute('ui:stophiding');
-			});
-		},
-		_build_square = function(pos){
+		}),
+		_add_square = function(pos){
 			var cellSize = app.request('config', 'cellSize'),
 			x = Math.floor(pos.x / cellSize) * cellSize,
-			y = Math.floor(pos.y / cellSize) * cellSize,
-			square = new Kinetic.Rect({
-				width: cellSize,
-				height: cellSize,
-				x: x,
-				y: y,
-				fill: 'grey',
-				stroke: 'grey',
-				strokeWidth: 1,
-				opacity: .5
-			});
-			_init_square(square);
-			return square;
+			y = Math.floor(pos.y / cellSize) * cellSize;
+			squares.push([x, y, x + cellSize, y + cellSize]);
+		},
+		_remove_square = function(pos){
+			var cellSize = app.request('config', 'cellSize'),
+			x = Math.floor(pos.x / cellSize) * cellSize,
+			y = Math.floor(pos.y / cellSize) * cellSize;
 		};
+
+		hideShape.on('mousedown', function(e){
+			if (app.request('config', 'toolMode') === 'reveal'){
+				this.remove();
+				isRevealing = true;
+				app.request('layer','reveal').draw();
+				app.vent.trigger('reveal:update');
+			}
+			return true;
+		});
+		hideShape.on('mousemove', function(e){
+			if (isRevealing){
+				this.remove();
+				app.request('layer', 'reveal').draw();
+				app.vent.trigger('reveal:update');
+			}
+		});
+		hideShape.on('mouseup', function(e){
+			app.execute('ui:stophiding');
+		});
 		app.commands.setHandler('reveal:add', function(pos){
 			var revealLayer = app.request('layer', 'reveal');
-			revealLayer.add(_build_square(pos));
+			_add_square(pos);
+			if (revealLayer.getChildren().length === 0) revealLayer.add(hideShape);
 			revealLayer.draw();
 			app.vent.trigger('reveal:update');
 		});
 		app.commands.setHandler('reveal:all', function(){
 			var revealLayer = app.request('layer', 'reveal');
-			revealLayer.removeChildren();
+			squares = [];
 			revealLayer.draw();
 			app.vent.trigger('reveal:update');
 			app.execute('figure:reveal:all');
@@ -64,12 +80,13 @@ define(['app', 'kinetic', 'module/layers'], function(app, Kinetic){
 				gridWidth = app.request('config', 'gridWidth'),
 				gridHeight = app.request('config', 'gridHeight'),
 				w, h;
-			revealLayer.removeChildren();
+			squares = [];
 			for (w = 0; w < gridWidth * cellSize; w += cellSize){
 				for (h = 0; h < gridHeight * cellSize; h += cellSize){
-					revealLayer.add(_build_square({x: w, y: h}));
+					_add_square({x: w, y: h});
 				}
 			}
+			if (revealLayer.getChildren().length === 0) revealLayer.add(hideShape);
 			revealLayer.draw();
 			app.vent.trigger('reveal:update');
 			app.execute('figure:reveal:none');
@@ -78,7 +95,7 @@ define(['app', 'kinetic', 'module/layers'], function(app, Kinetic){
 			isRevealing = false;
 		});
 		app.vent.on('layer:before:load:reveal', function(){
-			app.request('layer','reveal').getChildren(_init_square);
+			//app.request('layer','reveal').getChildren(_init_square);
 		});
 	});
 });
